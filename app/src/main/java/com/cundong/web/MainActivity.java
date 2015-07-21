3cd6db7cfd9b5aa9a20c7b0d087a6aa0a2dd6b5c
cundong <cundong.liu@gmail.com>
2015-07-21 16:45:14 +0800

    update

--- app/src/main/java/com/cundong/web/MainActivity.java
+++ app/src/main/java/com/cundong/web/MainActivity.java
@@ -25,6 +25,25 @@ import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+
+ 步骤：
+ 
+ 1.
+ WebView
+ onMeasure() 大小都设置为MeasureSpec.UNSPECIFIED
+
+ 2.
+ 调用WebView的layout()方法，重新布局。
+
+ 3.
+ 重新布局之后，获取当前webView的：
+ int measuredWidth = mWebview.getMeasuredWidth();
+ int measuredHeight = mWebview.getMeasuredHeight();
+
+ 4.
+ 根据获取的长宽，创建一个空的Bitmap，通过使用Canvas，把webView的内容draw到这个Bitmap上
+ */
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton mOpenLinkBtn;
